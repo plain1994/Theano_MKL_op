532d37e4765848d94cabd2256e8d6c5e1cf795f7
plain1994 <plain1994@gmail.com>
2017-07-19 16:24:17 +0800

    Extend add/mul for multi-dimensions inputs

--- mkl_elementwise_mul_op.py
+++ mkl_elementwise_mul_op.py
@@ -1,6 +1,6 @@
-import theano
 from theano import tensor, gof
 import numpy
+
 
 class ElementwiseMultiply(gof.Op):
     __props__ = ()
@@ -10,16 +10,24 @@ class ElementwiseMultiply(gof.Op):
         inputs = list(map(tensor.as_tensor_variable, inputs))
 
         if len(inputs) != 2:
-            raise TypeError('ElementwiseAdd: 2 arguments required, %d given' %len(inputs))
+            raise TypeError(
+                'ElementwiseMultiply: 2 arguments required, %d given'
+                % len(inputs))
 
-        if inputs[0].ndim != 2:
-            raise TypeError('ElementwiseAdd: x must be a 2-d matrix')
-        if inputs[1].ndim != 2:
-            raise TypeError('ElementwiseAdd: y must be a 2-d matrix')
+        if inputs[0].ndim > 5:
+            raise TypeError("ElementwiseMultiply: A's dimension is to large")
+        if inputs[1].ndim > 5:
+            raise TypeError("ElementwiseMultiply: B's dimension is to large")
+        if inputs[0].ndim != inputs[1].ndim:
+            raise TypeError(
+                "ElementwiseMultiply: \
+                elements in A and B should have same dimension")
 
         if inputs[0].dtype != inputs[1].dtype:
-            raise TypeError('ElementwiseAdd: elements in A and B should have same dtype')
-        
+            raise TypeError(
+                'ElementwiseMultiply: \
+                elements in A and B should have same dtype')
+
         output_var = inputs[0].type()
 
         return gof.Apply(self, inputs, [output_var])
@@ -31,18 +39,18 @@ class ElementwiseMultiply(gof.Op):
     def c_code(self, node, name, inputs, outputs, sub):
         if node.inputs[0].dtype is 'float32':
             ccode_type = 'float'
-            ccode_mulfunc = 'vsMul'
+            ccode_addfunc = 'vsMul'
         elif node.inputs[0].dtype is 'float64':
             ccode_type = 'double'
-            ccode_mulfunc = 'vdMul'
+            ccode_addfunc = 'vdMul'
         else:
-            raise TypeError('ElementwiseAdd: dtype %s is not supported.'
+            raise TypeError('ElementwiseMultiply: dtype %s is not supported.'
                             % (node.inputs[0].dtype))
-
-
 
         x, y = inputs
         z, = outputs
+
+        dim_x = node.inputs[0].ndim
 
         dtype_x = node.inputs[0].dtype
         dtype_y = node.inputs[1].dtype
@@ -52,18 +60,11 @@ class ElementwiseMultiply(gof.Op):
         itemsize_y = numpy.dtype(dtype_y).itemsize
         itemsize_z = numpy.dtype(dtype_z).itemsize
 
-
-        
-
-
         typenum_z = numpy.dtype(dtype_z).num
 
         fail = sub['fail']
 
-
-
         c_code = """
-
         // Validate that the output storage exists.
         if (NULL == %(z)s)
         {
@@ -71,7 +72,7 @@ class ElementwiseMultiply(gof.Op):
             Decrease received reference's ref count and allocate new
             output variable */
             Py_XDECREF(%(z)s);
-            %(z)s = (PyArrayObject*)PyArray_EMPTY(2,
+            %(z)s = (PyArrayObject*)PyArray_EMPTY(%(dim_x)s,
                                                 PyArray_DIMS(%(x)s),
                                                 %(typenum_z)s,
                                                 0);
@@ -81,27 +82,22 @@ class ElementwiseMultiply(gof.Op):
             }
         }
 
-        //int num1 = PyArray_DIMS(%(x)s)[0];
-        //printf("num1: %%d \\n", num1);
-        //int num2 = PyArray_DIMS(%(x)s)[1];
-        //printf("num2: %%d \\n", num2);
-	    //printf(\"%%llp, %%llp, %%llp\\n\", %(x)s, %(y)s, %(z)s);
+        //Get the number of elements to be calculated from PyArray_DIMS
+        int i;
+        int cal_num = 1;
+        for (i = 0; i < %(dim_x)s; i++){
+            cal_num *= PyArray_DIMS(%(x)s)[i];
+        }
 
-        //Perform the MKL Add (matrix elementwise add)
+        //Perform the MKL Mul (elementwise mul)
         %(ccode_type)s *px = (%(ccode_type)s*)PyArray_DATA(%(x)s);
         %(ccode_type)s *py = (%(ccode_type)s*)PyArray_DATA(%(y)s);
         %(ccode_type)s *pz = (%(ccode_type)s*)PyArray_DATA(%(z)s);
 
-
-        %(ccode_mulfunc)s(PyArray_DIMS(%(x)s)[0]*PyArray_DIMS(%(x)s)[1], px, py, pz);
+        %(ccode_addfunc)s(cal_num, px, py, pz);
         """
 
         return c_code % locals()
-
-
-
-
-
 
     def c_code_cache_version(self):
         return (1, 0, 0)
--- test_mkl_add.py
+++ test_mkl_add.py
@@ -2,37 +2,168 @@ import theano
 from theano import tensor
 import numpy
 import mkl_elementwise_add_op
-import unittest 
+import unittest
+
 
 class testMklAdd(unittest.TestCase):
-    def testFloat(self):
+    def test_Float(self):
         x0 = tensor.fmatrix('x0')
         y0 = tensor.fmatrix('y0')
         z0 = mkl_elementwise_add_op.ElementwiseAdd()(x0, y0)
         f0 = theano.function([x0, y0], z0)
         a0 = numpy.random.rand(6, 8).astype(numpy.float32)
         b0 = numpy.random.rand(6, 8).astype(numpy.float32)
-        #print(a0)
-        #print(b0)
+        # print(a0)
+        # print(b0)
         o0 = f0(a0, b0)
-        #print(o0)
+        # print(o0)
         self.assertTrue(numpy.allclose(o0, a0+b0))
 
-    def testDouble(self):
+    def test_Double(self):
         x1 = tensor.dmatrix('x1')
         y1 = tensor.dmatrix('y1')
         z1 = mkl_elementwise_add_op.ElementwiseAdd()(x1, y1)
         f1 = theano.function([x1, y1], z1)
-        theano.printing.pydotprint(f1, outfile='mkl_add.png', var_with_name_simple=True)
+        theano.printing.pydotprint(
+            f1, outfile='mkl_add.png', var_with_name_simple=True)
         a1 = numpy.random.rand(6, 8).astype(numpy.float64)
         b1 = numpy.random.rand(6, 8).astype(numpy.float64)
-        #print(a1)
-        #print(b1)
+        # print(a1)
+        # print(b1)
         o1 = f1(a1, b1)
-        #print(o1)
+        # print(o1)
         self.assertTrue(numpy.allclose(o1, a1+b1))
-    
+
+    def test_1D_dvector(self):
+        x = tensor.dvector('x')
+        y = tensor.dvector('y')
+
+        z = mkl_elementwise_add_op.ElementwiseAdd()(x, y)
+        f = theano.function([x, y], z)
+
+        a = numpy.random.rand(6).astype(numpy.float64)
+        b = numpy.random.rand(6).astype(numpy.float64)
+
+        o = f(a, b)
+        self.assertTrue(numpy.allclose(o, a+b))
+
+    def test_2D_dmatrix(self):
+        x = tensor.dmatrix('x')
+        y = tensor.dmatrix('y')
+
+        z = mkl_elementwise_add_op.ElementwiseAdd()(x, y)
+        f = theano.function([x, y], z)
+
+        a = numpy.random.rand(6, 7).astype(numpy.float64)
+        b = numpy.random.rand(6, 7).astype(numpy.float64)
+
+        o = f(a, b)
+        self.assertTrue(numpy.allclose(o, a+b))
+
+    def test_3D_dtensor(self):
+        x = tensor.dtensor3('x')
+        y = tensor.dtensor3('y')
+
+        z = mkl_elementwise_add_op.ElementwiseAdd()(x, y)
+        f = theano.function([x, y], z)
+
+        a = numpy.random.rand(6, 7, 8).astype(numpy.float64)
+        b = numpy.random.rand(6, 7, 8).astype(numpy.float64)
+
+        o = f(a, b)
+        self.assertTrue(numpy.allclose(o, a+b))
+
+    def test_4D_dtensor(self):
+        x = tensor.dtensor4('x')
+        y = tensor.dtensor4('y')
+
+        z = mkl_elementwise_add_op.ElementwiseAdd()(x, y)
+        f = theano.function([x, y], z)
+
+        a = numpy.random.rand(6, 7, 8, 9).astype(numpy.float64)
+        b = numpy.random.rand(6, 7, 8, 9).astype(numpy.float64)
+
+        o = f(a, b)
+        self.assertTrue(numpy.allclose(o, a+b))
+
+    def test_5D_dtensor(self):
+        x = tensor.dtensor5('x')
+        y = tensor.dtensor5('y')
+
+        z = mkl_elementwise_add_op.ElementwiseAdd()(x, y)
+        f = theano.function([x, y], z)
+
+        a = numpy.random.rand(6, 7, 8, 9, 10).astype(numpy.float64)
+        b = numpy.random.rand(6, 7, 8, 9, 10).astype(numpy.float64)
+
+        o = f(a, b)
+        self.assertTrue(numpy.allclose(o, a+b))
+
+    def test_1D_fvector(self):
+        x = tensor.fvector('x')
+        y = tensor.fvector('y')
+
+        z = mkl_elementwise_add_op.ElementwiseAdd()(x, y)
+        f = theano.function([x, y], z)
+
+        a = numpy.random.rand(6).astype(numpy.float32)
+        b = numpy.random.rand(6).astype(numpy.float32)
+
+        o = f(a, b)
+        self.assertTrue(numpy.allclose(o, a+b))
+
+    def test_2D_fMatrix(self):
+        x = tensor.fmatrix('x')
+        y = tensor.fmatrix('y')
+
+        z = mkl_elementwise_add_op.ElementwiseAdd()(x, y)
+        f = theano.function([x, y], z)
+
+        a = numpy.random.rand(6, 7).astype(numpy.float32)
+        b = numpy.random.rand(6, 7).astype(numpy.float32)
+
+        o = f(a, b)
+        self.assertTrue(numpy.allclose(o, a+b))
+
+    def test_3D_ftensor(self):
+        x = tensor.ftensor3('x')
+        y = tensor.ftensor3('y')
+
+        z = mkl_elementwise_add_op.ElementwiseAdd()(x, y)
+        f = theano.function([x, y], z)
+
+        a = numpy.random.rand(6, 7, 8).astype(numpy.float32)
+        b = numpy.random.rand(6, 7, 8).astype(numpy.float32)
+
+        o = f(a, b)
+        self.assertTrue(numpy.allclose(o, a+b))
+
+    def test_4D_ftensor(self):
+        x = tensor.ftensor4('x')
+        y = tensor.ftensor4('y')
+
+        z = mkl_elementwise_add_op.ElementwiseAdd()(x, y)
+        f = theano.function([x, y], z)
+
+        a = numpy.random.rand(6, 7, 8, 9).astype(numpy.float32)
+        b = numpy.random.rand(6, 7, 8, 9).astype(numpy.float32)
+
+        o = f(a, b)
+        self.assertTrue(numpy.allclose(o, a+b))
+
+    def test_5D_dtensor(self):
+        x = tensor.ftensor5('x')
+        y = tensor.ftensor5('y')
+
+        z = mkl_elementwise_add_op.ElementwiseAdd()(x, y)
+        f = theano.function([x, y], z)
+
+        a = numpy.random.rand(6, 7, 8, 9, 10).astype(numpy.float32)
+        b = numpy.random.rand(6, 7, 8, 9, 10).astype(numpy.float32)
+
+        o = f(a, b)
+        self.assertTrue(numpy.allclose(o, a+b))
 
 
-if __name__ =='__main__': 
-  unittest.main()
+if __name__ == '__main__':
+    unittest.main()
